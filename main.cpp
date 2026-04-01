dfdb805e3356ae29e5d5aae17e0e486272032c33
Sim <rubberbandlunchbox@gmail.com>
2026-04-01 16:56:10 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -5,18 +5,26 @@ using namespace std;
 
 int N,M;
 vector<int> vec;
-
-void func(int cnt) {
-
-
-
-
-
+int arr[9];
+void func(int cnt, int start) {
+	if (cnt == M){
+		for (int i=0; i<M; i++){
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = start; i <= N; i++){
+		arr[cnt] = i;
+		func(cnt+1, i+1);
+	}
 
 
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(0, 1);
+
+   return 0;
 }
